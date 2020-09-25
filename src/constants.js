ebcf4168f8ce1e7d16e4eef148be2a550bc141e4
Ahamed <ahamedali95@gmail.com>
2020-09-24 22:03:29 -0400

    first commit

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const pokemonFetchRequestActionTypes = (() => {
-  return {
-    POKEMON_FETCH_REQUEST: 'POKEMON_FETCH_REQUEST',
-    POKEMON_FETCH_SUCCESS: 'POKEMON_FETCH_SUCCESS',
-    POKEMON_FETCH_FAILURE: 'POKEMON_FETCH_FAILURE'
-  }
-})();
